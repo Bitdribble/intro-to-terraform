1e93ebf0e326fe3a8977c319b51c68a247c42193
Andrei Radulescu-Banu <andreiradulescu-banu@isee.ai>
2022-07-22 14:58:56 -0400

    Development

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -1,110 +1,154 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# DEPLOY A SINGLE EC2 INSTANCE
-# This template runs a simple "Hello, World" web server on a single EC2 Instance
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-terraform {
-  # This module is now only being tested with Terraform 1.1.x. However, to make upgrading easier, we are setting 1.0.0 as the minimum version.
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "< 4.0"
-    }
-  }
-}
-
-# ------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ------------------------------------------------------------------------------
-
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
+variable "databricks_workspace" {
+  type        = string
+  description = "Name of the Databricks workspace"
+  default     = "isee_perception"
+}
+
+
+resource "aws_vpc" "workspace-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "${var.name_prefix}_vpc"
+    Name = "${var.databricks_workspace}_vpc"
   }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+
+resource "aws_internet_gateway" "workspace-vpc-gw" {
+  vpc_id = aws_vpc.workspace-vpc.id
 
   tags = {
-    Name = "${var.name_prefix}_vpc_igw"
+    Name = "${var.databricks_workspace}_vpc_igw"
   }
 }
 
 
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "workspace-vpc-rt" {
+  vpc_id = aws_vpc.workspace-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.workspace-vpc-gw.id
   }
 
   tags = {
-    Name = "${var.name_prefix}_vpc_igw"
+    Name = "${var.databricks_workspace}_vpc_igw"
   }
 }
 
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.workspace-vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1f"
 
   tags = {
-    Name = "${var.name_prefix}_subnet"
+    Name = "${var.databricks_workspace}_public"
   }
 }
 
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.workspace-vpc-rt.id
 }
 
 
-resource "aws_network_interface" "main" {
-  subnet_id       = aws_subnet.main.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.main.id]
+resource "aws_security_group" "sg" {
+  name        = "${var.databricks_workspace}_allow_web_and_ssh"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.workspace-vpc.id
+
+  ingress {
+    description      = "HTTPS Traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP Traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH Traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "ICMP Traffic"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
-    Name = "${var.name_prefix}_if"
-  }  
-}
-
-resource "aws_eip" "main" {
-  vpc                       = true
-  network_interface         = aws_network_interface.main.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on                = [aws_internet_gateway.main]
-
-  tags = {
-    Name = "${var.name_prefix}_eip"
+    Name = "${var.databricks_workspace}_allow_web_and_ssh"
   }
 }
 
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.public-subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.sg.id]
 
-# ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY A SINGLE EC2 INSTANCE
-# ---------------------------------------------------------------------------------------------------------------------
+  tags = {
+    Name = "${var.databricks_workspace}_web_server_nic"
+  }  
+}
 
-resource "aws_instance" "main" {
-  ami                    = "ami-0cff7528ff583bf9a"
-  instance_type          = "t2.micro"
+resource "aws_eip" "web_server_eip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.workspace-vpc-gw]
+
+  tags = {
+    Name = "${var.databricks_workspace}_eip"
+  }
+}
+
+output "web_server_eip" {
+  value = aws_eip.web_server_eip.public_ip
+  description = "Web server elastic interface IP"
+}
+
+output "web_server_private_ip" {
+  value = aws_instance.web_server_instance.private_ip
+}
+
+
+resource "aws_instance" "web_server_instance" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t3.micro"
+  availability_zone = "us-east-1f"
 
   key_name          = "isee-dev-andrei"
   network_interface {
-    network_interface_id = aws_network_interface.main.id
+    network_interface_id = aws_network_interface.web-server-nic.id
     device_index         = 0
   }
   
@@ -118,24 +162,7 @@ resource "aws_instance" "main" {
   EOF
 
   tags = {
-    Name = "${var.name_prefix}_ws"
-  }
+    Name = "${var.databricks_workspace}_web_server"
+  }  
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "aws_security_group" "main" {
-  name = "terraform-example-instance"
-  description = "Allow web inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  # Inbound HTTP from anywhere
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
